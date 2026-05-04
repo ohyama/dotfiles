c4dade18826b17642b43b578cc7b22f96ef483a2
ohyama <me@ohyama.io>
2026-05-04 14:19:36 +0900

    feat: oh-my-zsh を導入

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+
+# chezmoi
 brew install chezmoi
+
+# 1Password
 brew install --cask 1password
 
 echo "Sign in to 1Password desktop and the App Store before proceeding."
