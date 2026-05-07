f6eb3ef58d7799bdf3b370d9ea6ac17556b23bee
ohyama <me@ohyama.io>
2026-05-07 10:41:02 +0900

    chore: コミットメッセージのフォーマットを変更

--- home/private_dot_config/nvim/lua/config/autocmds.lua
+++ home/private_dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "gitcommit",
+  callback = function()
+    vim.opt_local.spell = false
+    vim.opt_local.formatoptions:append("t") -- textwidth で自動改行
+    vim.opt_local.formatoptions:append("m") -- マルチバイト文字の境界で改行可能
+    vim.opt_local.formatoptions:append("M") -- マルチバイト結合時にスペースを挿入しない
+  end,
+})
